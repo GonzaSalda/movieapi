65a2725077af0de64a628dbda4fb309bdd1cf8dd
GonzaSalda <gonza.salda00@gmail.com>
2024-07-29 00:18:54 -0300

    add movie search functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
-function App() {
+//Api key
+const API_URL = "http://www.omdbapi.com/?apikey=4650ae25"
+
+
+const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json();
+    console.log(data)
+  }
+
+  useEffect(() => {
+     searchMovies("Dead pool")
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
